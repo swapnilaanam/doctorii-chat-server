de0858d0cc65db9f32a1ffce2aae447065e80dc8
swapnilaanam <swapnilaanam211@gmail.com>
2024-04-18 04:19:20 +0600

    some changes

--- index.js
+++ index.js
@@ -7,22 +7,10 @@ const { Server } = require('socket.io');
 const { createServer } = require('http');
 
 const app = express();
-
-app.use(cors());
-
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "X-Requested-With");
-    res.header("Access-Control-Allow-Headers", "Content-Type");
-    res.header("Access-Control-Allow-Methods", "PUT, GET, POST, DELETE, OPTIONS");
-    next();
-});
-
-
 const server = createServer(app);
 const io = new Server(server, {
     cors: {
-        origin: 'http://localhost:3000',
+        origin: '*',
         methods: ['GET', 'POST'],
         credentials: true
     }
@@ -45,9 +33,9 @@ io.on('connection', (socket) => {
     });
 });
 
-// app.get('/', (req, res) => {
-//     res.send('Doctorii Chat Server Is Running....');
-// });
+app.get('/', (req, res) => {
+    res.send('Doctorii Chat Server Is Running....');
+});
 
 server.listen(port, () => {
     console.log(`Server is listening to Port: ${port}`);
