39f7eeeccea017534b7bed5e6553f0a4c5ab2159
swapnilaanam <swapnilaanam211@gmail.com>
2024-04-18 03:13:10 +0600

    added a backed server for chat system

---
+++ index.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+const port = process.env.PORT || 4000;
+
+const { Server } = require('socket.io');
+const { createServer } = require('http');
+
+const app = express();
+const server = createServer(app);
+const io = new Server(server, {
+    cors: {
+        origin: 'http://localhost:3000',
+        methods: ['GET', 'POST'],
+        credentials: true
+    }
+});
+
+io.on('connection', (socket) => {
+    console.log(`Socket ${socket.id} is connected.`);
+
+    socket.on('send-message', ({roomName, msg}) => {
+        io.to(roomName).emit("receive-message", {roomName, msg});
+    });
+
+    socket.on('join-room', (roomName) => {
+        socket.join(roomName);
+        console.log(`${socket.id} joined the room`);
+    });
+
+    socket.on('disconnect', () => {
+        console.log(`Socket ${socket.id} is disconnected...`);
+    });
+})
+
+server.listen(port, () => {
+    console.log(`Server is listening to Port: ${port}`);
+});
