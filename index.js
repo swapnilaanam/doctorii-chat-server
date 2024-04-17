3fb69cc0b86ef4075fb0a2702a032e4bcd29b71e
swapnilaanam <swapnilaanam211@gmail.com>
2024-04-18 04:09:04 +0600

    some changes

--- index.js
+++ index.js
@@ -10,6 +10,14 @@ const app = express();
 
 app.use(cors());
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    res.header("Access-Control-Allow-Headers", "Content-Type");
+    res.header("Access-Control-Allow-Methods", "PUT, GET, POST, DELETE, OPTIONS");
+    next();
+});
+
 
 const server = createServer(app);
 const io = new Server(server, {
